391c295d470e3b4ed9a35ae2768e8ccddd1b527e
hoonglily <hoonglily4@gmail.com>
2024-02-09 12:54:34 -0800

    getting api working

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=-122.4194&appid=4c61663ffd6468e18773db8e75bdcca4`;
   return (
     <div className="App">
     </div>
