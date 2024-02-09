441ffe5df6b3d9989daf2bd014b89e9e3d477cde
hoonglily <hoonglily4@gmail.com>
2024-02-09 13:07:02 -0800

    added .env

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function App() {
 
-  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=-122.4194&appid=4c61663ffd6468e18773db8e75bdcca4`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=-122.4194&appid=API_KEY`;
   return (
     <div className="App">
     </div>
